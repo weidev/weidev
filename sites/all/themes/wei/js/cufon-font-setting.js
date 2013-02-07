08a3d4420406bf9708a1f3f9ee09927ac3c007f6
Drupal developer <drupal.drupaldevelopment@gmail.com>
2013-02-07 18:06:44 +0530

    First commit

---
+++ sites/all/themes/wei/js/cufon-font-setting.js
@@ -0,0 +1,14 @@
+//Cufon.replace('h1', { fontFamily: 'Vegur' });
+//Cufon.replace('h2', { fontFamily: 'Myriad Pro' });
+
+//GothamLight, GothamBook, GothamBold, MyriadProLightCond, MyriadProLight, MyriadProRegular, MyriadProSemibold, Foglihten
+
+Cufon.replace('#navigation ul li a', { fontFamily: 'GothamLight', hover: 'true' });
+Cufon.replace('.rooms-rate-btn, #block-block-11, #header_top #block-menu-menu-top-left-menu, #header_top #block-menu-menu-top-right-menu, #header_top #block-block-2, .topSect .topPart a, .botPart .rhtSect ul.links li a, .midSect .textConWrap .rhtSect .btn, ul.footer-nav li a, #footer .midSect .ttl h3, #footer .midSect .ttl .num, .breadcum ul li a, .postCont h2, .postCont h3, .small-banner a.reserv-btn, ul.info-list li a .name, ul.link-list-3 li a, .breadcum .breadcum h6, #slides h5, #slides .slide ul.tabs li a, .link-btn a', { fontFamily: 'GothamBook', hover: 'true' });
+Cufon.replace('.banner .slide .textPart b, .textContent h3, .textContentIn h1, .small-banner h3', { fontFamily: 'Foglihten' });
+Cufon.replace('ul.link-list-1 li a', { fontFamily: 'MyriadProSemibold', hover: 'true' });
+Cufon.replace('.textContent .lftSet .halfSect h6, .textContent .lftSet p, ul.accordian-menu li a', { fontFamily: 'MyriadProLight', hover: 'true' });
+Cufon.replace('#footer .midSect .textBox h5, #footer .midSect .textBox h4 .name', { fontFamily: 'MyriadProSemibold'});
+Cufon.replace('ul.link-list-2 li a, #footer .midSect .textBox p, .small-banner p, .postCont p, .breadcum-diff ul li a', { fontFamily: 'MyriadProRegular', hover: 'true' });
+Cufon.replace('ul.gall-box-list li .gall-box h4', { fontFamily: 'MyriadProLightCond'});
+Cufon.replace('.breadcum ul li.active a', { fontFamily: 'GothamBold'});
